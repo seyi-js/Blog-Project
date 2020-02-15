f0cf3218daad88c1f7940b7433d432833292104a
ADEBAYO SAMUEL OLUWASEYI <adebayosamueljahsmine30@gmail.com>
2020-02-15 11:33:21 +0100

    deployment

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const mongoose =  require('mongoose');
 const MongoStore = require('connect-mongo')(session)
 const PORT = process.env.PORT || 80;
 mongoose.set('useCreateIndex', true);
-const url = 'mongodb://localhost:27017/blogDB';
+const url = 'mongodb+srv://samuelseyi:OLUWASEYI50886@cluster0-ss5ul.mongodb.net/test?retryWrites=true&w=majority';
 // const url = process.env.MONGO_URL
 // // const uri = ""
 // //const uri = process.env.ATLAS_URI;
